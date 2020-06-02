41923fdc72ff310083c7f7147fbd181081668e65
Alexandr G <s207883@yandex.ru>
2020-06-02 19:29:29 +0300

    feat: add core layer

---
+++ Wallet.Core/Enums/RepoActionsResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Wallet.Core.Enums
+{
+	/// <summary>
+	/// Результат работы репозитория.
+	/// </summary>
+	public enum RepoActionsResult
+	{
+		/// <summary>
+		/// Ошибка.
+		/// </summary>
+		Fail,
+
+		/// <summary>
+		/// Успех.
+		/// </summary>
+		Success
+	}
+}
