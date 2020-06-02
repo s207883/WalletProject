8e3390c7b274d4fbc5dad68dfea7b90fc96762f4
Alexandr G <s207883@yandex.ru>
2020-06-02 23:12:03 +0300

    fix: comment fix

--- Wallet.BLL/Interfaces/IBankAccountRepository.cs
+++ Wallet.BLL/Interfaces/IBankAccountRepository.cs
@@ -28,13 +28,16 @@ namespace Wallet.BLL.Interfaces
 		/// <returns>Результат работы и новое состояние счета.</returns>
 		Task<(BankRepoActionResults Result, BankAccountViewModel newValue)> DescreaseAmount(int accountId, long amount);
 
+
 		/// <summary>
 		/// Перевести деньги из одной валюты в другую.
 		/// </summary>
-		/// <param name="accointId">Номер счета.</param>
-		/// <param name="currencyId">Идентификатор валюты.</param>
-		/// <param name="amountToExhange">Количество валюты для обмена.</param>
 		/// <returns>Результат и новое состояние счета.</returns>
+		/// <param name="walletId">Идентификатор кошелька</param>
+		/// <param name="originCurrencyId">Идентификатор валюты, которую нужно поменять.</param>
+		/// <param name="destinationCurrencyId">Идентификатор валюты, на которую нужно поменять.</param>
+		/// <param name="amountToExhange">Количество валюты для обмена.</param>
+		/// <returns>Результат работы и новое состояние счетов.</returns>
 		Task<(BankRepoActionResults Result, IEnumerable<BankAccountViewModel> bankAccounts)> ExchangeCurrencyAsync(int walletId, int originCurrencyId, int destinationCurrencyId, long amountToExhange);
 	}
 }
