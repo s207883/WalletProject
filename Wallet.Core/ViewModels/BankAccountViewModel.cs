7f6bb546edcd797e30e8a574865d8a3e953d14f4
Alexandr G <s207883@yandex.ru>
2020-06-02 19:35:15 +0300

    feat: add view models

---
+++ Wallet.Core/ViewModels/BankAccountViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wallet.Core.ViewModels
+{
+	/// <summary>
+	/// Модель представления счета.
+	/// </summary>
+	public class BankAccountViewModel
+	{
+		/// <summary>
+		/// Название валюты.
+		/// </summary>
+		public string CurrencyName { get; set; }
+
+		/// <summary>
+		/// Количество.
+		/// </summary>
+		public long Amount { get; set; }
+	}
+}
---
+++ Wallet.Core/ViewModels/UserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wallet.Core.ViewModels
+{
+	/// <summary>
+	/// Модель представления пользователя.
+	/// </summary>
+	public class UserViewModel
+	{
+		/// <summary>
+		/// Имя пользователя.
+		/// </summary>
+		public string UserName { get; set; }
+
+		/// <summary>
+		/// Аккаунты пользователя.
+		/// </summary>
+		public IEnumerable<BankAccountViewModel> BankAccounts { get; set; }
+	}
+}
