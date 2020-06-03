e835b378d53d13de55d3c698a52f23ea17bc2e59
Alexandr G <s207883@yandex.ru>
2020-06-03 10:58:31 +0300

    feat: user repo implement

---
+++ Wallet.Core/Enums/UserRepoActionsResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Wallet.Core.Enums
+{
+	/// <summary>
+	/// Результат работы репозитория пользователя.
+	/// </summary>
+	public enum UserRepoActionsResult
+	{
+		/// <summary>
+		/// Успех.
+		/// </summary>
+		Success,
+
+		/// <summary>
+		/// Не обнаружен пользователь.
+		/// </summary>
+		IdentityNotFound,
+
+		/// <summary>
+		/// Нерпвильный идентификатор.
+		/// </summary>
+		WrongIdentity,
+	}
+}
